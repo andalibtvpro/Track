52f8240379def42a02b1ede8bc3e1a1c9a9e02a5
andalibtvpro <155395432+andalibtvpro@users.noreply.github.com>
2024-12-06 18:46:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ messaging.onMessage((payload) => {
 
   new Notification(notificationTitle, notificationOptions);
 });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered:', registration);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
